7fc150e5f34d5f4076b1658d705da0631fe47ce4
Ouroboros <ouroroboros88@gmail.com>
2026-05-08 22:40:01 +0800

    adsfasdf

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def chat_with_PepetGPT(prompt):
         print("PROMPT:", prompt)
 
         response = client.models.generate_content(
-            model="models/gemma-4-26b-a4b-it",
+            model="models/gemma-4-31b-it",
             contents=prompt
         )
 
