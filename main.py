6fa4d268f8cc3d0579144a2b90f5eee101dc3efd
Ouroboros <ouroroboros88@gmail.com>
2026-05-08 22:25:17 +0800

    change model

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def chat_with_PepetGPT(prompt):
         print("PROMPT:", prompt)
 
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemma-3-4b-it",
             contents=prompt
         )
 
